b6a0ff760d0ad3ee918851bd5494eed8ca6b1ae0
Martin Velev <martin@blurrmc.com>
2020-04-21 19:31:25 -0500

    Made the bottom bar of the record view disappear and added a back button for it also.

--- Blurred-ios/RecordViewController.swift
+++ Blurred-ios/RecordViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class RecordViewController: UIViewController {
 
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tabBarController?.tabBar.isHidden = true
         // Do any additional setup after loading the view.
     }
     
+ 
 
     /*
     // MARK: - Navigation

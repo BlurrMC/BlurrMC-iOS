fbe9a25cb545a624618eb9aac32cacfe33239e55
Martin Velev <martin@blurrmc.com>
2020-04-23 13:18:40 -0500

    Changed license and added some improvements to the record screen.

--- Blurred-ios/RecordViewController.swift
+++ Blurred-ios/RecordViewController.swift
@@ -7,17 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordViewController: UIViewController {
 
- 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = true
-        // Do any additional setup after loading the view.
+         // Do any additional setup after loading the view.
     }
-    
- 
 
     /*
     // MARK: - Navigation

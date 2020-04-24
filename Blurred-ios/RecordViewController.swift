df3eaff292872f7c8c3fef7ea042743a22e7d926
Martin Velev <martin@blurrmc.com>
2020-04-23 20:32:51 -0500

    Last recording screen commit until I have device to test it.

--- Blurred-ios/RecordViewController.swift
+++ Blurred-ios/RecordViewController.swift
@@ -7,24 +7,11 @@
 //
 
 import UIKit
-import AVFoundation
 
 class RecordViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = true
-         // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+

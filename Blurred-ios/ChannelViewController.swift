b844a31b67b09ba87447396782ddafdee6494888
Martin Velev <martin@blurrmc.com>
2020-05-14 19:52:20 -0500

    Added profile picture api.

--- Blurred-ios/ChannelViewController.swift
+++ Blurred-ios/ChannelViewController.swift
@@ -11,7 +11,7 @@ import SwiftKeychainWrapper
 import Nuke
 import SwiftUI
 
-class ChannelViewController: UIViewController { // Look at youself. Look at what you have done.
+class ChannelViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate { // Look at youself. Look at what you have done.
     
     
 
@@ -26,6 +26,9 @@ class ChannelViewController: UIViewController { // Look at youself. Look at what
         let lineView = UIView(frame: CGRect(x: 0, y: 220, width: self.view.frame.size.width, height: 1))
         lineView.backgroundColor = UIColor.black
         self.view.addSubview(lineView)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ChannelViewController.imageTapped(gesture:)))
+        avatarImage.isUserInteractionEnabled = true
+        avatarImage.addGestureRecognizer(tapGesture)
         self.followersLabel.isUserInteractionEnabled = true
         self.followingLabel.isUserInteractionEnabled = true
         let tap = UITapGestureRecognizer(target: self, action: #selector(ChannelViewController.tapFunction))
@@ -35,6 +38,14 @@ class ChannelViewController: UIViewController { // Look at youself. Look at what
         loadMemberChannel()
         
         // Setup the view so you can integerate it right away with the channel api.
+    }
+    @objc func imageTapped(gesture: UIGestureRecognizer) {
+        // if the tapped view is a UIImageView then set it to imageview
+        if (gesture.view as? UIImageView) != nil {
+            print("Image Tapped")
+            pickAvatar()
+
+        }
     }
     @objc func tapFunction(sender:UITapGestureRecognizer) {
         print("tap working")
@@ -105,7 +116,35 @@ class ChannelViewController: UIViewController { // Look at youself. Look at what
                 }
         }
         task.resume()
-    } // I will set this up later.
+    } // I will set this up later
+    
+    func importImage() {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = true
+        self.present(image, animated: true) {
+            
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            avatarImage.image = image
+            postRequest()
+        } else {
+            self.showUnkownError()
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    func takePicture() {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.camera
+        image.allowsEditing = true
+        self.present(image, animated: true) {
+            
+        }
+    }
     func showErrorContactingServer() {
 
         // create the alert
@@ -127,6 +166,103 @@ class ChannelViewController: UIViewController { // Look at youself. Look at what
 
         // show the alert
         self.present(alert, animated: true, completion: nil)
+    }
+    func showUnkownError() {
+
+        // create the alert
+        let alert = UIAlertController(title: "Error", message: "We don't know what happend wrong here! Try again later.", preferredStyle: UIAlertController.Style.alert)
+
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "Fine", style: UIAlertAction.Style.default, handler: nil))
+
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    func pickAvatar() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Avatar", message: "Change your avatar.", preferredStyle: UIAlertController.Style.actionSheet)
+
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "Pick from gallery", style: UIAlertAction.Style.default, handler: { action in
+                print("Pick from gallery")
+                self.importImage()
+            }))
+            alert.addAction(UIAlertAction(title: "Take photo", style: UIAlertAction.Style.default, handler: { action in
+                print("Take photo")
+                self.takePicture()
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+        }
+        // create the alert
+        
+    }
+    func postRequest(){
+    // the image in UIImage type
+        guard let image = avatarImage else { return  }
+
+    let filename = "avatar.png"
+
+    // generate boundary string using a unique per-app string
+    let boundary = UUID().uuidString
+
+    let fieldName = "reqtype"
+    let fieldValue = "fileupload"
+
+    let fieldName2 = "userhash"
+    let fieldValue2 = "caa3dce4fcb36cfdf9258ad9c"
+
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config)
+
+    // Set the URLRequest to POST and to the specified URL
+    var urlRequest = URLRequest(url: URL(string: "http://10.0.0.2:3000")!)
+    urlRequest.httpMethod = "POST"
+
+    // Set Content-Type Header to multipart/form-data, this is equivalent to submitting form data with file upload in a web browser
+    // And the boundary is also set here
+    urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+    var data = Data()
+
+    // Add the reqtype field and its value to the raw http request data
+    data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+    data.append("Content-Disposition: form-data; name=\"\(fieldName)\"\r\n\r\n".data(using: .utf8)!)
+    data.append("\(fieldValue)".data(using: .utf8)!)
+
+    // Add the userhash field and its value to the raw http reqyest data
+    data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+    data.append("Content-Disposition: form-data; name=\"\(fieldName2)\"\r\n\r\n".data(using: .utf8)!)
+    data.append("\(fieldValue2)".data(using: .utf8)!)
+
+    // Add the image data to the raw http request data
+    data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+    data.append("Content-Disposition: form-data; name=\"fileToUpload\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
+    data.append("Content-Type: image/png\r\n\r\n".data(using: .utf8)!)
+    data.append(image.image!.pngData()!)
+
+    // End the raw http request data, note that there is 2 extra dash ("-") at the end, this is to indicate the end of the data
+    // According to the HTTP 1.1 specification https://tools.ietf.org/html/rfc7230
+    data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+
+    // Send a POST request to the URL, with the data we created earlier
+    session.uploadTask(with: urlRequest, from: data, completionHandler: { responseData, response, error in
+        
+        if(error != nil){
+            print("\(error!.localizedDescription)")
+        }
+        
+        guard let responseData = responseData else {
+            print("no response data")
+            return
+        }
+        
+        if let responseString = String(data: responseData, encoding: .utf8) {
+            print("uploaded to: \(responseString)")
+        }
+    }).resume()
     }
     // This checks in with the api and makes sure the token is right and then with the id it goes to the id's (or current user's) channel.
 

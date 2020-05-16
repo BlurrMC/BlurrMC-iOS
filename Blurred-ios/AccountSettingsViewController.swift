bea84ca0494609b7b7648d7254b65d4168a8950c
Martin Velev <martin@blurrmc.com>
2020-05-16 09:46:00 -0500

    Added new view controller.

---
+++ Blurred-ios/AccountSettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AccountSettingsViewController.swift
+//  Blurred-ios
+//
+//  Created by Martin Velev on 5/16/20.
+//  Copyright © 2020 BlurrMC. All rights reserved.
+//
+
+import UIKit
+
+class AccountSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@
 * Changed SwiftKeychainWrapper to Valet (Should fix the keychain issue)
 * Fixed bug where timer would continue when user would log out.
 * Added "nothing here" on follower list and following list when a user doesn't have any followers or any one they are following.
+* Added account settings view controller (Doesn't actually contact the api. Just placeholder for now.)
 ## v0.2 (PRERELEASE)
 * Some design changes
 * Added most channel attrbitues that update (except followers and following)
